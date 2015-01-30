71aae3bcf4de34d368647525824d51873ffeec4e
DaveLancaster <lancaster.dave@gmail.com>
2015-01-30 15:57:46 +0000

    New slice() syntax

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(os,core)]
+#![feature(io,os,core,collections)]
 
 extern crate getopts;
 extern crate regex;
@@ -31,7 +31,7 @@ fn build_magic_packet(mac: String) -> Result<Vec<u8>, &'static str> {
 
     for _ in range(0u8, 16) {
         match payload.len() {
-            6 => packet.push_all(payload.slice(0,6)),
+            6 => packet.push_all(&payload[0 .. 6]),
             _ => return Err("invalid buffer length"),
         };
     }
@@ -55,8 +55,8 @@ fn send_magic_packet(packet: Vec<u8>, laddr: SocketAddr, raddr: String) -> Resul
         Err(e) => panic!("could not bind socket: {}", e),
     };
 
-    let result = socket.send_to(packet.slice(0,102),(raddr.as_slice(), 9u16));
-        
+    let result = socket.send_to(&packet[0 .. 102],(raddr.as_slice(), 9u16));  
+ 
     return result
 }
 
