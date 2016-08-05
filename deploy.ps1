eaa925323dc6674b2e1e122866ce5629371c7721
dave <lancaster.dave@gmail.com>
2016-08-05 10:27:36 +0100

    change dir again

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 Echo "Starting...";
 
-cd C:\appveyor\projects\wol-rst\;
+cd C:\appveyor\projects\wol-rst\applications\wol\;
 Echo $pwd;
 
 Import-Module -Name .\Tunable-SSL-Validator\TunableSSLValidator.psm1;
