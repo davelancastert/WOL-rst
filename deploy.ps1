9668fa91dcea355be15349453b7ddfd2dca571eb
dave <lancaster.dave@gmail.com>
2016-08-05 11:37:09 +0100

    no path

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,3 @@
 Echo "Starting...";
 
-cd C:\appveyor\projects\wol-rst\applications\wol\;
-Echo $pwd;
-
-Import-Module -Name c:\appveyor\projects\wol-rst\applications\wol\Tunable-SSL-Validator\TunableSSLValidator.psm1;
+Import-Module -Name .\Tunable-SSL-Validator\TunableSSLValidator.psm1;
